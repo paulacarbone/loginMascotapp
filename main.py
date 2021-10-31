d82a204d89195a09a93bbd3488aaf5bc6c482c7d
leonor perez <leonor perez>
2021-10-31 14:47:05 -0300

    fix login , se agrega bbdd

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import re
 
 app = Flask(__name__)
 
-# Change this to your secret key (can be anything, it's for extra protection)
 app.secret_key = 'your secret key'
 
-# Enter your database connection details below
+# database connection 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
@@ -18,13 +17,11 @@ app.config['MYSQL_DB'] = 'pythonlogin'
 mysql = MySQL(app)
 
 @app.route('/pythonlogin/', methods=['GET', 'POST'])
-
 def login():
-    # Output message if something goes wrong...
+   
     msg = ''
     # Check if "username" and "password" POST requests exist (user submitted form)
     if request.method == 'POST' and 'usuario' in request.form and 'password' in request.form:
-        # Create variables for easy access
         usuario = request.form['usuario']
         password = request.form['password']
         # Check if account exists using MySQL
@@ -39,30 +36,25 @@ def login():
             session['id'] = account['id']
             session['usuario'] = account['usuario']
             # Redirect to home page
-            return redirect(url_for('home'))
-
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
-    # Show the login form with message (if any)
+            return redirect(url_for('home'))       
+        else:  
+            msg = 'Usuario/password Incorrecto!'
     return render_template('index.html', msg=msg)
+   
 
 @app.route('/pythonlogin/logout')
 def logout():
-    # Remove session data, this will log the user out
    session.pop('loggedin', None)
    session.pop('id', None)
    session.pop('usuario', None)
-   # Redirect to login page
+ 
    return redirect(url_for('login'))
 
 @app.route('/pythonlogin/register', methods=['GET', 'POST'])
 def register():
-    # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
     if request.method == 'POST' and 'nombre' in request.form and 'apellido' in request.form and 'localidad' in request.form and 'usuario' in request.form and 'password' in request.form:
-        # Create variables for easy access
         nombre = request.form['nombre']
         apellido = request.form['apellido']
         localidad = request.form['localidad']
@@ -73,32 +65,30 @@ def register():
         account = cursor.fetchone()
         # If account exists show error and validation checks
         if account:
-            msg = 'Account already exists!'
+            msg = 'El usuario ya existe!'
         elif not usuario or not password:
             msg = 'Please fill out the form!'
         else:
             # Account doesnt exists and the form data is valid, now insert new account into accounts table
             cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s)', (nombre, apellido, localidad, usuario, password,))
             mysql.connection.commit()
-            msg = 'You have successfully registered!'
+            msg = 'Usuario registrado correctamente!'
     elif request.method == 'POST':
         # Form is empty... (no POST data)
         msg = 'Please fill out the form!'
-    # Show registration form with message (if any)
     return render_template('register.html', msg=msg)   
 
 @app.route('/pythonlogin/home')
 def home():
     # Check if user is loggedin
     if 'loggedin' in session:
-        # User is loggedin show them the home page
         return render_template('home.html', usuario=session['usuario'])
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))  
 
 
 if __name__ == "__main__":
-    app.run(port=3306, debug=True)
+    app.run(port=3000, debug=True)
 
 
     
---
+++ utils/accounts.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 31, 2021 at 06:40 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pythonlogin`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `apellido` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `localidad` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `usuario` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `nombre`, `apellido`, `localidad`, `usuario`, `password`) VALUES
+(1, 'Martin', 'Galvan', 'Cañuelas', 'm.galva', '1234'),
+(2, 'leonor', 'perez', 'saenz peña', 'lperez', '1234'),
+(4, 'mara', 'vazquez', 'caba', 'maralaura', '1234'),
+(5, 'Caro', 'lopez', 'caba', 'clopez', '12345'),
+(6, 'lola', 'mora', 'san isidro', 'lmora', '1234');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nombre` (`nombre`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
