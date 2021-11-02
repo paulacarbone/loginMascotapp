f2ae44682bb0e2716872965cec483b6861367747
Paula Carbone <paulacamilacarbone@gmail.com>
2021-11-02 01:45:22 -0300

    subiendo icono y background

--- main.py
+++ main.py
@@ -7,35 +7,35 @@ app = Flask(__name__)
 
 app.secret_key = 'your secret key'
 
-# database connection 
+ 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'pythonlogin'
 
-# Intialize MySQL
+
 mysql = MySQL(app)
 
 @app.route('/pythonlogin/', methods=['GET', 'POST'])
 def login():
    
     msg = ''
-    # Check if "username" and "password" POST requests exist (user submitted form)
+    
     if request.method == 'POST' and 'usuario' in request.form and 'password' in request.form:
         usuario = request.form['usuario']
         password = request.form['password']
-        # Check if account exists using MySQL
+        
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE usuario = %s AND password = %s', (usuario, password,))
-        # Fetch one record and return result
+        
         account = cursor.fetchone()
-        # If account exists in accounts table in out database
+        
         if account:
-            # Create session data, we can access this data in other routes
+            
             session['loggedin'] = True
             session['id'] = account['id']
             session['usuario'] = account['usuario']
-            # Redirect to home page
+            
             return redirect(url_for('home'))       
         else:  
             msg = 'Usuario/password Incorrecto!'
@@ -53,7 +53,7 @@ def logout():
 @app.route('/pythonlogin/register', methods=['GET', 'POST'])
 def register():
     msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    
     if request.method == 'POST' and 'nombre' in request.form and 'apellido' in request.form and 'localidad' in request.form and 'usuario' in request.form and 'password' in request.form:
         nombre = request.form['nombre']
         apellido = request.form['apellido']
@@ -63,32 +63,32 @@ def register():
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('SELECT * FROM accounts WHERE usuario = %s', (usuario,))
         account = cursor.fetchone()
-        # If account exists show error and validation checks
+        
         if account:
             msg = 'El usuario ya existe!'
         elif not usuario or not password:
             msg = 'Please fill out the form!'
         else:
-            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            
             cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s, %s, %s)', (nombre, apellido, localidad, usuario, password,))
             mysql.connection.commit()
             msg = 'Usuario registrado correctamente!'
     elif request.method == 'POST':
-        # Form is empty... (no POST data)
+        
         msg = 'Please fill out the form!'
     return render_template('register.html', msg=msg)   
 
 @app.route('/pythonlogin/home')
 def home():
-    # Check if user is loggedin
+    
     if 'loggedin' in session:
         return render_template('home.html', usuario=session['usuario'])
-    # User is not loggedin redirect to login page
+    
     return redirect(url_for('login'))  
 
 
 if __name__ == "__main__":
-    app.run(port=3000, debug=True)
+    app.run(port=3306, debug=True)
 
 
     
